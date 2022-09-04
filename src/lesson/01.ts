7e1ac63e5bd024e84d9a555f2a8611a0e75b24ec
steve zhang <871506263@qq.com>
2022-09-04 22:22:14 +0800

    first commit

---
+++ src/lesson/01.ts
@@ -0,0 +1,6 @@
+
+let createdByNewBoolean: Boolean = new Boolean(1);
+console.log('createdByNewBoolean :>> ', createdByNewBoolean);
+let createdByBoolean: boolean = Boolean(1);
+console.log('createdByBoolean :>> ', createdByBoolean);
+export default {};
