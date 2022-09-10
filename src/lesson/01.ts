65335706a2daacf9811f7fcae2c40609e6c50fff
steve zhang <871506263@qq.com>
2022-09-10 21:24:46 +0800

    提交代码于: 2022-09-10 21:24:46

--- src/lesson/01.ts
+++ src/lesson/01.ts
@@ -1,6 +1,21 @@
 
+//1.boolean
+/* let isDone: boolean = false;
 let createdByNewBoolean: Boolean = new Boolean(1);
 console.log('createdByNewBoolean :>> ', createdByNewBoolean);
 let createdByBoolean: boolean = Boolean(1);
-console.log('createdByBoolean :>> ', createdByBoolean);
+console.log('createdByBoolean :>> ', createdByBoolean); */
+
+
+// 2.number
+let decLiteral: number = 6;
+let hexLiteral: number = 0xf00d;
+// ES6 中的二进制表示法
+let binaryLiteral: number = 0b1010;
+// ES6 中的八进制表示法
+let octalLiteral: number = 0o744;
+let notANumber: number = NaN;
+let infinityNumber: number = Infinity;
+console.log('111');
+
 export default {};
