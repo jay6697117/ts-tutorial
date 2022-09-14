7f25c123e20d5db55c8ef76d977c28c025045848
steve zhang <871506263@qq.com>
2022-09-14 10:13:20 +0800

    提交代码于: 2022-09-14 10:13:20

--- src/lesson/01.ts
+++ src/lesson/01.ts
@@ -1,11 +1,9 @@
-
 //1.boolean 布尔值
 /* let isDone: boolean = false;
 let createdByNewBoolean: Boolean = new Boolean(1);
 console.log('createdByNewBoolean :>> ', createdByNewBoolean);
 let createdByBoolean: boolean = Boolean(1);
 console.log('createdByBoolean :>> ', createdByBoolean); */
-
 
 // 2.number 数值
 /*
@@ -21,14 +19,28 @@ console.log('111');
 */
 
 // 3.string字符串
+/*
 let myName: string = 'Tom';
 let myAge: number = 25;
-console.log('myName:', myName)
-console.log('myAge:', myAge)
+console.log('myName:', myName);
+console.log('myAge:', myAge);
 
 // 模板字符串
 let sentence: string = `Hello, my name is ${myName}.
 I'll be ${myAge + 1} years old next month.`;
-console.log('sentence', sentence)
+console.log('sentence', sentence);
+*/
+
+// 4.空值
+
+function alertName(): void {
+  alert('My name is Tom');
+  return undefined;
+}
+
+let unuseable1:void = undefined;
+// let unuseable2:void = null;
+console.log('unuseable1', unuseable1)
+// console.log('unuseable2', unuseable2)
 
 export default {};
