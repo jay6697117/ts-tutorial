f570dc3d7d108598d58047cdd16e817c4a068763
steve zhang <871506263@qq.com>
2022-09-14 09:49:03 +0800

    提交代码于: 2022-09-14 09:49:03

--- output/lesson/01.js
+++ output/lesson/01.js
@@ -1,10 +1,10 @@
-//1.boolean
+//1.boolean 布尔值
 /* let isDone: boolean = false;
 let createdByNewBoolean: Boolean = new Boolean(1);
 console.log('createdByNewBoolean :>> ', createdByNewBoolean);
 let createdByBoolean: boolean = Boolean(1);
 console.log('createdByBoolean :>> ', createdByBoolean); */
-// 2.number
+// 2.number 数值
 var decLiteral = 6;
 var hexLiteral = 0xf00d;
 // ES6 中的二进制表示法
@@ -14,4 +14,11 @@ var octalLiteral = 484;
 var notANumber = NaN;
 var infinityNumber = Infinity;
 console.log('111');
+// 3.string字符串
+var myName = 'Tom';
+var myAge = 25;
+console.log('myName', myName);
+console.log('myAge', myAge);
+// 模板字符串
+var sentence = "Hello, my name is " + myName + ".\nI'll be " + (myAge + 1) + " years old next month.";
 export default {};
--- src/lesson/01.ts
+++ src/lesson/01.ts
@@ -1,5 +1,5 @@
 
-//1.boolean
+//1.boolean 布尔值
 /* let isDone: boolean = false;
 let createdByNewBoolean: Boolean = new Boolean(1);
 console.log('createdByNewBoolean :>> ', createdByNewBoolean);
@@ -7,7 +7,7 @@ let createdByBoolean: boolean = Boolean(1);
 console.log('createdByBoolean :>> ', createdByBoolean); */
 
 
-// 2.number
+// 2.number 数值
 let decLiteral: number = 6;
 let hexLiteral: number = 0xf00d;
 // ES6 中的二进制表示法
@@ -17,5 +17,16 @@ let octalLiteral: number = 0o744;
 let notANumber: number = NaN;
 let infinityNumber: number = Infinity;
 console.log('111');
+
+
+// 3.string字符串
+let myName: string = 'Tom';
+let myAge: number = 25;
+console.log('myName', myName)
+console.log('myAge', myAge)
+
+// 模板字符串
+let sentence: string = `Hello, my name is ${myName}.
+I'll be ${myAge + 1} years old next month.`;
 
 export default {};
