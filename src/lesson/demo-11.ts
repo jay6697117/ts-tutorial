943c111cad40ffcf97021e599046099a8d790785
steve zhang <871506263@qq.com>
2022-10-28 11:22:50 +0800

    提交代码于: 2022-10-28 11:22:50

--- src/lesson/demo-11.ts
+++ src/lesson/demo-11.ts
@@ -1,14 +1,15 @@
 export {};
 
 // 内置对象
-// 六.泛型参数的默认类型
-// 在 TypeScript 2.3 以后，我们可以为泛型中的类型参数指定默认类型。当使用泛型时没有在代码中直接指定类型参数，从实际值参数中也无法推测出时，这个默认类型就会起作用。
-function createArray<T = string>(length: number, value: T): T[] {
-  let result: T[] = [];
-  for (let i = 0; i < length; i++) {
-    result[i] = value;
-  }
-  return result;
-}
+// 1.ECMAScript 的内置对象
+interface BooleanCopy extends Boolean {}
+class BooleanCopy1 extends Boolean {}
 
-console.log(createArray(5, 'hello'));
+let b1: BooleanCopy = new Boolean(1);
+let b2: Boolean = new Boolean(1);
+let b3: Boolean = new BooleanCopy1(1);
+let b4: BooleanCopy1 = new BooleanCopy1(1);
+console.log('b1', b1);
+console.log('b2', b2);
+console.log('b3', b3);
+console.log('b4', b4)
