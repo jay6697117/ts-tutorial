418ee1c89a33a50ccfcb4dcdd99b9dbb527c3b72
steve zhang <871506263@qq.com>
2022-10-28 10:53:50 +0800

    提交代码于: 2022-10-28 10:53:50

--- src/lesson/demo-11.ts
+++ src/lesson/demo-11.ts
@@ -1,1 +1,14 @@
 export {};
+
+// 内置对象
+// 六.泛型参数的默认类型
+// 在 TypeScript 2.3 以后，我们可以为泛型中的类型参数指定默认类型。当使用泛型时没有在代码中直接指定类型参数，从实际值参数中也无法推测出时，这个默认类型就会起作用。
+function createArray<T = string>(length: number, value: T): T[] {
+  let result: T[] = [];
+  for (let i = 0; i < length; i++) {
+    result[i] = value;
+  }
+  return result;
+}
+
+console.log(createArray(5, 'hello'));
