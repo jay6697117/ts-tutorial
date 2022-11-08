0ca54326a341c1c8890570e0611986e0381ee6e4
steve zhang <871506263@qq.com>
2022-11-08 10:32:20 +0800

    提交代码于: 2022-11-08 10:32:20

--- class语法糖实现.js
+++ class语法糖实现.js
@@ -1,36 +1,38 @@
-// var __extends =
-//   (this && this.__extends) ||
-//   function (d, b) {
-//     for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
-//     function __() {
-//       this.constructor = d;
-//     }
-//     d.prototype = b === null ? Object.create(b) : ((__.prototype = b.prototype), new __());
-//   };
+var __extends =
+  (this && this.__extends) ||
+  function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() {
+      this.constructor = d;
+    }
+    d.prototype = b === null ? Object.create(b) : ((__.prototype = b.prototype), new __());
+  };
 
-// var Animal = (function () {
-//   function Animal(name) {
-//     this.name = name;
-//   }
-//   return Animal;
-// })();
+var Animal = (function () {
+  function Animal(name) {
+    this.name = name;
+  }
+  return Animal;
+})();
 
-// var Cat = (function (_super) {
-//   console.log('arguments 1:', arguments)
-//   __extends(Cat, _super);
-//   function Cat() {
-//     console.log('arguments 2:', arguments)
-//     console.log('_super', _super)
-//     _super.apply(this, arguments);
-//   }
-//   Cat.prototype.sayHi = function () {
-//     console.log('Meow, My name is ' + this.name);
-//   };
-//   return Cat;
-// })(Animal);
+var Cat = (function (_super) {
+  console.log('arguments 1:', arguments)
+  __extends(Cat, _super);
+  function Cat() {
+    console.log('arguments 2:', arguments)
+    console.log('_super', _super)
+    _super.apply(this, arguments);
+  }
+  Cat.prototype.sayHi = function () {
+    console.log('Meow, My name is ' + this.name);
+  };
+  return Cat;
+})(Animal);
 
-// var cat = new Cat('Tom');
-// console.log('cat :>> ', cat);
+var cat = new Cat('Tom');
+console.log('cat :>> ', cat);
+
+console.log('------------------------------------------');
 
 const foo1 = function (param1, param2) {
   console.log('param1 :', param1);
@@ -49,3 +51,4 @@ const foo2 = (function (fn) {
   return foo3;
 })(foo1);
 foo2(1, 2);
+console.log('------------------------------------------');
--- src/lesson/demo-15.ts
+++ src/lesson/demo-15.ts
@@ -1,2 +1,62 @@
 export {};
-console.log('111');
+
+// 注意不要混淆了 TypeScript 中的 => 和 ES6 中的 =>。
+// 在 TypeScript 的 "类型定义" 中，=> 用来表示函数的定义，左边是输入类型，需要用括号括起来，右边是输出类型。
+// 在 ES6 中，=> 叫做箭头函数，应用十分广泛，可以参考 ES6 中的箭头函数。
+
+interface Alarm {
+  alertFn: () => string;
+  // alertFn(): string;
+}
+
+interface Light {
+  lightOn: () => void;
+  lightOff: () => void;
+  // lightOn(): void;
+  // lightOff(): void;
+}
+
+class Door {
+  saiHi(): string {
+    return '我是门';
+  }
+}
+
+class SecurityDoor extends Door implements Alarm {
+  saiHi1 = (): string => {
+    return '我是防盗门1';
+  };
+  saiHi(): string {
+    return '我是防盗门2';
+  }
+  alertFn(): string {
+    return 'alert 哔啵哔啵1～～～';
+  }
+}
+
+class Car implements Alarm,Light {
+  alertFn(): string {
+    return 'alert 哔啵哔啵2～～～';
+  }
+  lightOn():void{
+    console.log('灯开了～～～');
+  }
+  lightOff():void{
+    console.log('灯关了～～～');
+  }
+}
+
+const d = new Door();
+const sd = new SecurityDoor();
+const c = new Car();
+
+console.log('d :>> ', d);
+console.log('sd :>> ', sd);
+console.log(d.saiHi());
+console.log(sd.saiHi1());
+console.log(sd.saiHi());
+console.log(sd.alertFn());
+console.log(c.alertFn());
+c.lightOn();
+c.lightOff();
+// 上例中，Car 实现了 Alarm 和 Light 接口，既能报警，也能开关车灯。
