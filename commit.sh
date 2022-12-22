cf3b39fe3ec7e08a9b3bb223217342a89ec10d23
steve zhang <871506263@qq.com>
2022-12-22 11:24:13 +0800

    zhangjinhui提交代码于: 2022-12-22 11:24:13

--- commit.sh
+++ commit.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #输入./push命令提交代码，只需要info变量中配置提交信息
 current=`date "+%Y-%m-%d %H:%M:%S"`
-info=【${USER}】"提交代码于: "$current
+info=${USER}"提交代码于: "$current
 #切到当前绝对路径
 cd `pwd`
 git add -A
