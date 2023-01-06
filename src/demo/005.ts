8757e3a1fda858a3935c06bf64eee2a9b50eca36
steve zhang <871506263@qq.com>
2023-01-06 14:27:29 +0800

    zhangjinhui提交代码于: 2023-01-06 14:27:29

--- src/demo/005.ts
+++ src/demo/005.ts
@@ -8,9 +8,15 @@ export function printResult(param: any, tag: string = 'h1'): void {
 }
 
 // 1.函数的类型
-const foo = (name: string, age: number = 18): string => {
-  return `我是${name}, 我今年${age}岁`;
+// const foo = (name: string, age: number = 18): string => {
+//   return `我是${name}, 我今年${age}岁`;
+// };
+// printResult(foo('张金辉'));
+// printResult(foo('李世民', 20));
+
+//通过?表示该参数为可选参数
+const fn = (name: string, age?: number): string => {
+  return name + age;
 };
 
-printResult(foo('张金辉'));
-printResult(foo('李世民', 38));
+printResult(fn('张三'));
