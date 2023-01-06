3f621e1a6c1aee5cb6f26f3587f1e4e197bc2ec9
steve zhang <871506263@qq.com>
2023-01-06 10:57:16 +0800

    zhangjinhui提交代码于: 2023-01-06 10:57:16

--- src/demo/005.ts
+++ src/demo/005.ts
@@ -7,35 +7,10 @@ export function printResult(param: any, tag: string = 'h1'): void {
   document.body.appendChild(hr);
 }
 
-interface ObjectTemplate {
-  /** The initial value of Object.prototype.constructor is the standard built-in Object constructor. */
-  constructor: Function;
+// 1.函数的类型
+const foo = (name: string, age: number = 18): string => {
+  return `我是${name}, 我今年${age}岁`;
+};
 
-  /** Returns a string representation of an object. */
-  toString(): string;
-
-  /** Returns a date converted to a string using the current locale. */
-  toLocaleString(): string;
-
-  /** Returns the primitive value of the specified object. */
-  valueOf(): Object;
-
-  /**
-   * Determines whether an object has a property with the specified name.
-   * @param v A property name.
-   */
-  hasOwnProperty(v: PropertyKey): boolean;
-
-  /**
-   * Determines whether an object exists in another object's prototype chain.
-   * @param v Another object whose prototype chain is to be checked.
-   */
-  isPrototypeOf(v: Object): boolean;
-
-  /**
-   * Determines whether a specified property is enumerable.
-   * @param v A property name.
-   */
-  propertyIsEnumerable(v: PropertyKey): boolean;
-}
-printResult('111');
+printResult(foo('张金辉'));
+printResult(foo('李世民', 38));
