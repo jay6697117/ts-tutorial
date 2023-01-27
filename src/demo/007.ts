dd019cc7211445c3b35958fe098f5e40f3c41b36
steve zhang <871506263@qq.com>
2023-01-27 23:08:31 +0800

    zhangjinhui提交代码于: 2023-01-27 23:08:31

--- src/demo/007.ts
+++ src/demo/007.ts
@@ -7,4 +7,151 @@ export function printResult(param: any, tag: string = 'h1'): void {
   document.body.appendChild(node);
   document.body.appendChild(hr);
 }
-printResult('111');
+
+// JavaScript 中有很多内置对象，它们可以直接在 TypeScript 中当做定义好了的类型。
+// ECMAScript 的内置对象
+// Boolean、Number、string、RegExp、Date、Error
+
+// let b: Boolean = new Boolean(1);
+// printResult(b);
+// let n: Number = new Number(true);
+// printResult(n);
+// let s: String = new String('哔哩哔哩关注小满zs-666');
+// printResult(s);
+// let d: Date = new Date();
+// printResult(d);
+// printResult(d.getTime());
+// let r: RegExp = /^1/;
+// console.log(r);
+// let e: Error = new Error('error!');
+// console.log(e);
+
+// DOM 和 BOM 的内置对象
+// Document、HTMLElement、Event、NodeList 等
+let body: HTMLElement = document.body;
+let allDiv: NodeList = document.querySelectorAll('div');
+//读取div 这种需要类型断言 或者加个判断应为读不到返回null
+let div: HTMLElement = document.querySelector('div') as HTMLDivElement;
+document.addEventListener('click',  function (e: MouseEvent) {});
+
+//dom元素的映射表
+interface HTMLElementTagNameMap {
+  a: HTMLAnchorElement;
+  abbr: HTMLElement;
+  address: HTMLElement;
+  applet: HTMLAppletElement;
+  area: HTMLAreaElement;
+  article: HTMLElement;
+  aside: HTMLElement;
+  audio: HTMLAudioElement;
+  b: HTMLElement;
+  base: HTMLBaseElement;
+  bdi: HTMLElement;
+  bdo: HTMLElement;
+  blockquote: HTMLQuoteElement;
+  body: HTMLBodyElement;
+  br: HTMLBRElement;
+  button: HTMLButtonElement;
+  canvas: HTMLCanvasElement;
+  caption: HTMLTableCaptionElement;
+  cite: HTMLElement;
+  code: HTMLElement;
+  col: HTMLTableColElement;
+  colgroup: HTMLTableColElement;
+  data: HTMLDataElement;
+  datalist: HTMLDataListElement;
+  dd: HTMLElement;
+  del: HTMLModElement;
+  details: HTMLDetailsElement;
+  dfn: HTMLElement;
+  dialog: HTMLDialogElement;
+  dir: HTMLDirectoryElement;
+  div: HTMLDivElement;
+  dl: HTMLDListElement;
+  dt: HTMLElement;
+  em: HTMLElement;
+  embed: HTMLEmbedElement;
+  fieldset: HTMLFieldSetElement;
+  figcaption: HTMLElement;
+  figure: HTMLElement;
+  font: HTMLFontElement;
+  footer: HTMLElement;
+  form: HTMLFormElement;
+  frame: HTMLFrameElement;
+  frameset: HTMLFrameSetElement;
+  h1: HTMLHeadingElement;
+  h2: HTMLHeadingElement;
+  h3: HTMLHeadingElement;
+  h4: HTMLHeadingElement;
+  h5: HTMLHeadingElement;
+  h6: HTMLHeadingElement;
+  head: HTMLHeadElement;
+  header: HTMLElement;
+  hgroup: HTMLElement;
+  hr: HTMLHRElement;
+  html: HTMLHtmlElement;
+  i: HTMLElement;
+  iframe: HTMLIFrameElement;
+  img: HTMLImageElement;
+  input: HTMLInputElement;
+  ins: HTMLModElement;
+  kbd: HTMLElement;
+  label: HTMLLabelElement;
+  legend: HTMLLegendElement;
+  li: HTMLLIElement;
+  link: HTMLLinkElement;
+  main: HTMLElement;
+  map: HTMLMapElement;
+  mark: HTMLElement;
+  marquee: HTMLMarqueeElement;
+  menu: HTMLMenuElement;
+  meta: HTMLMetaElement;
+  meter: HTMLMeterElement;
+  nav: HTMLElement;
+  noscript: HTMLElement;
+  object: HTMLObjectElement;
+  ol: HTMLOListElement;
+  optgroup: HTMLOptGroupElement;
+  option: HTMLOptionElement;
+  output: HTMLOutputElement;
+  p: HTMLParagraphElement;
+  param: HTMLParamElement;
+  picture: HTMLPictureElement;
+  pre: HTMLPreElement;
+  progress: HTMLProgressElement;
+  q: HTMLQuoteElement;
+  rp: HTMLElement;
+  rt: HTMLElement;
+  ruby: HTMLElement;
+  s: HTMLElement;
+  samp: HTMLElement;
+  script: HTMLScriptElement;
+  section: HTMLElement;
+  select: HTMLSelectElement;
+  slot: HTMLSlotElement;
+  small: HTMLElement;
+  source: HTMLSourceElement;
+  span: HTMLSpanElement;
+  strong: HTMLElement;
+  style: HTMLStyleElement;
+  sub: HTMLElement;
+  summary: HTMLElement;
+  sup: HTMLElement;
+  table: HTMLTableElement;
+  tbody: HTMLTableSectionElement;
+  td: HTMLTableDataCellElement;
+  template: HTMLTemplateElement;
+  textarea: HTMLTextAreaElement;
+  tfoot: HTMLTableSectionElement;
+  th: HTMLTableHeaderCellElement;
+  thead: HTMLTableSectionElement;
+  time: HTMLTimeElement;
+  title: HTMLTitleElement;
+  tr: HTMLTableRowElement;
+  track: HTMLTrackElement;
+  u: HTMLElement;
+  ul: HTMLUListElement;
+  var: HTMLElement;
+  video: HTMLVideoElement;
+  wbr: HTMLElement;
+}
