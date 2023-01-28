3b00e2fbfe2568812c0cbc564924c4485c6d0dd7
steve zhang <871506263@qq.com>
2023-01-28 14:44:00 +0800

    zhangjinhui提交代码于: 2023-01-28 14:44:00

--- src/demo/008.ts
+++ src/demo/008.ts
@@ -7,6 +7,7 @@ export function printResult(param: any, tag: string = 'h1'): void {
   document.body.appendChild(node);
   document.body.appendChild(hr);
 }
+
 // 1.ok！那我们在TS 是如何定义类的如下图
 /* class Person {
   name: string = '默认姓名'; //默认值
@@ -24,12 +25,15 @@ export function printResult(param: any, tag: string = 'h1'): void {
 const p = new Person('zjh', 33);
 console.log('p', p);
 p.run(); */
+
 // 2.类的修饰符
 // 总共有三个 public private protected
 class Person {
   public name: string;
   private age: number;
+  // public age: number;
   protected some: any;
+  // public some: any;
   constructor(name: string, ages: number, some: any) {
     this.name = name;
     this.age = ages;
@@ -38,16 +42,20 @@ class Person {
   run() {}
 }
 class Man extends Person {
-  constructor() {
-    super('张三', 18, 1);
-    console.log(this.some);
+  constructor(_name: string, _age: number, _some: any) {
+    // super('张三', 18, 1);
+    super(_name, _age, _some);
+    console.log('Man constructor some:', this.some);
   }
   create() {
-    console.log(this.some);
+    console.log('Man create:', this.some);
   }
 }
-let p = new Person('小满', 18, 1);
-let m = new Man();
+let p = new Person('人', 1, 'hello world');
+let m = new Man('zjh', 18, '我出世啦');
+console.log('p', p);
+console.log('m', m);
 console.log(p.name);
 // console.log(p.age);
+m.create();
 // console.log(m.some);
