f8d70145736f944123605f03170078d54d6fd6b4
steve zhang <871506263@qq.com>
2023-01-31 16:26:06 +0800

    zhangjinhui提交代码于: 2023-01-31 16:26:06

--- src/demo/012.ts
+++ src/demo/012.ts
@@ -63,12 +63,15 @@ const array: Array<Item> = [
   { age: 123, name: '2' },
   { age: 123, name: '3' }
 ];
+console.log('array :>> ', array);
 
 type mapTypes = string | number;
 const map: Map<mapTypes, mapTypes> = new Map();
 
 map.set('1', '王爷');
 map.set('2', '陆北');
+
+console.log('map :>> ', map);
 
 const obj = {
   aaa: 123,
@@ -77,14 +80,15 @@ const obj = {
 
 let set: Set<number> = new Set([1, 2, 3, 4, 5, 6]);
 // let it:Iterator<Item> = array[Symbol.iterator]()
-const gen = (erg: any): void => {
-  let it: Iterator<any> = erg[Symbol.iterator]();
+const gen = (array: any): void => {
+  let it: Iterator<Item> = array[Symbol.iterator]();
   let next: any = { done: false };
   while (!next.done) {
     next = it.next();
-    if (!next.done) {
-      console.log(next.value);
-    }
+    console.log('next :', next);
+    // if (!next.done) {
+    //   console.log('next :', next);
+    // }
   }
 };
 gen(array);
--- src/index.ts
+++ src/index.ts
@@ -28,4 +28,5 @@
 // import('./demo/009');
 // import('./demo/010');
 // import('./demo/011');
-import('./demo/012');
+// import('./demo/012');
+import('./demo/013');
